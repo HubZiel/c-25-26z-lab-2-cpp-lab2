4c5fd87f8d8a476f563e805e02668b7da6f8bc11
HubZiel <121490100+HubZiel@users.noreply.github.com>
2025-10-30 22:34:17 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,37 @@
-#pragma once
+#pragma onc#pragma once
 
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+
+private:
+    Resource* resource;
+
+public:
+    ResourceManager() { //kontruktor domyślny
+        resource = new Resource();
+    }
+    ~ResourceManager() { //destruktor
+        delete resource;
+    }
+    ResourceManager(const ResourceManager& other) : resource(new Resource(*other.resource)) {} //konstruktor kopiujący
+
+    ResourceManager& operator=(const ResourceManager& other) // operator przypisania kopiujący 
+    {
+        if (this != &other) {
+            delete resource;                          // zwolnienie starego zasobu
+            resource = new Resource(*other.resource); // stworzenie kopii
+        }
+        return *this;
+    }
+
+    double get() const { 
+        return resource->get();
+    }
+
+
+
+
+
 };
